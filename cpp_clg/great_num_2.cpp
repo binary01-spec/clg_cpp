4821104faaadbeac9ea193b28c4e812d14799588
fazil <67891255+binary01-spec@users.noreply.github.com>
2022-05-14 14:03:56 +0530

    Update great_num_2.cpp

--- cpp_clg/great_num_2.cpp
+++ cpp_clg/great_num_2.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+int max_num(int x, int y);
 
-int main(){
-    int a, b;
-    cout << "enter the number: "<< endl;
-    cin >> a;
-    cin >> b;
-    cout << max(a,b) << " is largest";
+int main()
+{
+    int x, y;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    cout << max_num(x, y) << " is largest";
+}
+
+int max_num(int x, int y){
+    return (x > y) ? x : y;
 }
